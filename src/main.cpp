92f193055560c3a610b8aabd34ddec5e76687eda
ahmadkhoirulhudamuhari <khoirulahmad843@gmail.com>
2026-03-29 14:41:05 +0700

    update main.cpp dan REPORT.md sesuai instruksi tugas

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <string> // Tambahan wajib supaya bisa menyimpan teks berspasi
 
 using namespace std;
 
 int main(int argc, char ** argv) {
-    cout << "Hello world!" << endl;
+    string nama;
+    
+    // Menampilkan tulisan ke layar
+    cout << "Silahkan inputkan nama Anda: ";
+    
+    // Mengambil inputan dari keyboard (pakai getline supaya spasi ikut terbaca)
+    getline(cin, nama);
+    
+    // Menampilkan output sesuai format tugas
+    cout << "Hello " << nama << endl;
 
     return 0;
 }
